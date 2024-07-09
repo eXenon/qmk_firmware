3a093d9b6ff32b220198835e9e0c316f8f19ebee
eXenon <xavier.nunn@protonmail.com>
2024-07-09 13:53:40 +0200

    v2 - wip

--- keyboards/planck/keymaps/exenon/keymap.c
+++ keyboards/planck/keymaps/exenon/keymap.c
@@ -29,7 +29,7 @@
 #include "rgb_matrix.h"
 
 // Define layers
-enum planck_layers { _AZERTY, _AZERTY_SYMBOL, _NUMPAD, _WEIRD };
+enum planck_layers { _AZERTY, _AZERTY_SYMBOL, _NUMPAD, _WEIRD, _NAVIGATION };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -45,10 +45,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * `-----------------------------------------------------------------------------------'
      */
     [_AZERTY] = LAYOUT_planck_grid(
-            KC_ESC,  KC_B,    FR_EACU, KC_P,    KC_O,  KC_Z,    KC_W,   KC_V,  KC_D,    KC_L,    KC_J,  KC_BSPC,
-            KC_LSFT, KC_A,    KC_U,    KC_I,    KC_E,  KC_M,    KC_C,   KC_T,  KC_S,    KC_R,    KC_N,  KC_ENT,
-            KC_TAB,  FR_EGRV, FR_AGRV, KC_Y,    KC_X,  FR_CCED, KC_K,   KC_Q,  KC_G,    KC_H,    KC_F,  FR_COLN,
-            MO(2),   KC_LCTL, KC_LALT, KC_LGUI, MO(1), KC_SPC,  KC_SPC, MO(3), KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT
+            KC_ESC,  FR_B,    FR_EACU, FR_P,    FR_O,  FR_Z,    FR_W,   FR_V,  FR_D,    FR_L,    FR_J,  KC_BSPC,
+            KC_LSFT, FR_A,    FR_U,    FR_I,    FR_E,  FR_M,    FR_C,   FR_T,  FR_S,    FR_R,    FR_N,  KC_ENT,
+            KC_TAB,  FR_EGRV, FR_AGRV, FR_Y,    FR_X,  FR_CCED, FR_K,   FR_Q,  FR_G,    FR_H,    FR_F,  MO(_NAVIGATION),
+            MO(_NUMPAD),   KC_LCTL, KC_LALT, KC_LGUI, MO(_AZERTY_SYMBOL), KC_SPC,  KC_SPC, MO(_WEIRD), KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT
     ),
 
     /* Write bépo when the system is configured in AZERTY - symbol layer
@@ -80,10 +80,53 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |      |      |      |      |      |             |      |   0  |      |   +  |      |
      * `-----------------------------------------------------------------------------------'
      */
-    [_AZERTY_SYMBOL] = LAYOUT_planck_grid(
+    [_NUMPAD] = LAYOUT_planck_grid(
             _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_VOLU, KC_VOLD, KC_P7,   KC_P8, KC_P9,   KC_PSLS, _______,
             _______, KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_MPRV, KC_MNXT, KC_P4,   KC_P5, KC_P6,   KC_PAST, _______,
             _______, KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_MPLY, KC_MUTE, KC_P1,   KC_P2, KC_P3,   KC_PMNS, FR_EQL,
             _______, _______, _______, _______, _______, _______, _______, _______, KC_P0, _______, KC_PPLS, _______
+    ),
+
+    /* Weird layer
+     * ,-----------------------------------------------------------------------------------.
+     * |      |  F1  |  F2  |  F3  |  F4  | Vol+ | Vol- |   7  |   8  |   9  |   /  |      |
+     * |------+------+------+------+------+-------------+------+------+------+------+------|
+     * |      |  F5  |  F6  |  F7  |  F8  | Prev | Next |   4  |   5  |   6  |   *  |      |
+     * |------+------+------+------+------+------|------+------+------+------+------+------|
+     * |      |  F9  |  F10 |  F11 |  F12 | Play | Mute |   1  |   2  |   3  |   -  |   =  |
+     * |------+------+------+------+------+------+------+------+------+------+------+------|
+     * |      |      |      |      |      |             |      |   0  |      |   +  |      |
+     * `-----------------------------------------------------------------------------------'
+     */
+    [_WEIRD] = LAYOUT_planck_grid(
+            _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_VOLU, KC_VOLD, KC_P7,   KC_P8, KC_P9,   KC_PSLS, _______,
+            _______, KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_MPRV, KC_MNXT, KC_P4,   KC_P5, KC_P6,   KC_PAST, _______,
+            _______, KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_MPLY, KC_MUTE, KC_P1,   KC_P2, KC_P3,   KC_PMNS, FR_EQL,
+            _______, _______, _______, _______, _______, _______, _______, _______, KC_P0, _______, KC_PPLS, _______
+    ),
+
+    /* Navigation layer
+     * ,-----------------------------------------------------------------------------------.
+     * |      |      |      |      |      |      |      |      |      |      |      |      |
+     * |------+------+------+------+------+-------------+------+------+------+------+------|
+     * |      |  PT  |  PP  |  NP  |  NT  |      |      |   ←  |   ↑  |   ↓  |   →  |      |
+     * |------+------+------+------+------+------|------+------+------+------+------+------|
+     * |      |      | Term | Brow |      |      |      | Home | PgUp | PgDn | End  |      |
+     * |------+------+------+------+------+------+------+------+------+------+------+------|
+     * |      |      |      |      |      |             |      |      |      |      |      |
+     * `-----------------------------------------------------------------------------------'
+     *
+     * PT   : Previous Tab  = Ctrl + Shift + Tab
+     * NT   : Next Tab      = Ctrl + Tab
+     * PP   : Previous Page = Cmd + Left
+     * NP   : Next Page     = Cmd + Right
+     * Term : Terminal      = Right-Cmd + K
+     * Brow : Browser       = Right-Cmd + L
+     */
+    [_NAVIGATION] = LAYOUT_planck_grid(
+            _______,            _______,       _______,        _______,      _______, _______, _______, _______,    _______,      _______,  _______, _______,
+            _______, LCTL(LSFT(KC_TAB)), LGUI(KC_LEFT), LGUI(KC_RIGHT), LCTL(KC_TAB), _______, _______, KC_LEFT,      KC_UP,      KC_DOWN, KC_RIGHT, _______,
+            _______,            _______,    LGUI(KC_K),     LGUI(KC_L),      _______, _______, _______, KC_HOME, KC_PAGE_UP, KC_PAGE_DOWN,   KC_END, _______,
+            _______,            _______,       _______,        _______,      _______, _______, _______, _______,    _______,      _______,  _______, _______
     )
 };
